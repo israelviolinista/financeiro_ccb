151173582b9ec1bad5875a31798957e5d280e970
israel <ueg.israel@gmail.com>
2024-08-16 11:33:13 -0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Teste PHP-FinanceiroCCB</title>
+    </head>
+    <body>
+        <?php echo "<p>Olá Mundo</p>"; ?>
+        <?php echo "<p>Teste site Israel</p>"; ?>
+    </body>
+</html>
